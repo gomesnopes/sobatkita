2a8c4f2ba5bdcd9f090e1e41f9ce707fa874a4d4
gomesnopes <fandigomes.abk@gmail.com>
2026-05-29 14:03:36 +0900

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,7 @@
 // supabase-config.js
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
-const supabaseUrl = 'YOUR_SUPABASE_URL';
-const supabaseKey = 'YOUR_SUPABASE_ANON_KEY';
+const supabaseUrl = 'https://twunssclwdvwdbjwsysc.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR3dW5zc2Nsd2R2d2RiandzeXNjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3MDExOTksImV4cCI6MjA5NTI3NzE5OX0.z_ME9KMif183URkPRs9JpRFhsudaNTLzojdmc7GuP1c';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
